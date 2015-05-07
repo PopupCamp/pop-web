b5c12661c7b452d6a8c1870223381df11229167e
Paul Cuciureanu <paulcu@gmail.com>
2015-05-06 21:29:21 -0400

    first french + english meter-based site, deployed at http://popupcamp.meteor.com

--- client/app.js
+++ client/app.js
@@ -1,5 +1,15 @@
-Meteor.startup(function() {
+Meteor.startup(function () {
   $('body').attr('id', 'page-top');
+  Session.set("showLoadingIndicator", true);
+
+  TAPi18n.setLanguage(getUserLanguage())
+    .done(function () {
+      Session.set("showLoadingIndicator", false);
+    })
+    .fail(function (error_message) {
+      // Handle the situation
+      console.log(error_message);
+    });
 });
 
 Template.landing.rendered = function() {
