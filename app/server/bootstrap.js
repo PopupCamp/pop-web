c6531b1d3029401839ec557089c36f2a52923894
Paul Cuciureanu <paulcu@gmail.com>
2015-06-15 23:38:33 -0400

    Adding SEO support

--- app/server/bootstrap.js
+++ app/server/bootstrap.js
@@ -1,2 +1,13 @@
 Meteor.startup(function () {
+  if (Meteor.isClient) {
+    return SEO.config({
+      title: 'Popup Camp',
+      meta: {
+        'description': 'Halte-garderie mobile événementielle / Mobile Child Care for Events'
+      },
+      og: {
+        'image': 'http://popupcamp.com/img/profile.png'
+      }
+    });
+  }
 });
